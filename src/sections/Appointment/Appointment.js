c1ff661dbecced206b80f7861cfed36068dbe236
abdulbiztech <testuse534@gmail.com>
2024-09-28 15:41:14 +0530

    new changes

--- src/sections/Appointment/Appointment.js
+++ src/sections/Appointment/Appointment.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Appointment.scss';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
-import {AiFillHome} from "react-icons/ai";
+import { AiFillHome } from "react-icons/ai";
 import ContactForm from '../../components/ContactForm/ContactForm';
 
 const Appointment = () => {
-
-    const mapLink = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158857.72810807265!2d-0.24168024584704212!3d51.52877184047419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f131!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sbd!4v1666254073802!5m2!1sen!2sbd'
+    // Update the map link to the new Mumbai location
+    const mapLink = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15073.40739305411!2d72.87704262968993!3d19.071682569180125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cf226ceeb271%3A0xe2fb255764b727c8!2sShop%20No.%205%2C%20Hi-tech%20Premises%20Co-op%20Hsg.%20Soc.%2C%20Vinoba%20Bhave%20Nagar%2C%20Mumbai%2C%20Maharashtra%20400070!5e0!3m2!1sen!2sin!4v1693483229927!5m2!1sen!2sin';
 
     return (
         <section className='appointment-section pb-70' data-aos="fade-up" data-aos-duration="2000">
@@ -14,20 +14,31 @@ const Appointment = () => {
                 <div className="row align-items-center">
                     <div className="col-lg-5 col-md-6">
                         <div className="google-map">
-                            <iframe title='map' src={mapLink}></iframe>
+                            {/* Embed Google Map with the updated link */}
+                            <iframe 
+                                title='map' 
+                                src={mapLink}
+                                width="100%"
+                                height="450"
+                                style={{ border: 0 }}
+                                allowFullScreen="" 
+                                loading="lazy"
+                                referrerPolicy="no-referrer-when-downgrade">
+                            </iframe>
 
                             <div className="location-name">
                                 <AiFillHome />
-                                <p>35 West Dental Street California 1004</p>
+                                <p>Shop No. 5, Hi-tech Premises Co-op Hsg. Soc., Vinoba Bhave Nagar, Mumbai, Maharashtra 400070</p>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-7 col-md-6">
                         <div className="appointment-form-area">
                             <SectionTitle 
-                            subTitle="BOOK APPOINTMENT"
-                            title="Care at Dentalist is pleasure"
-                            description="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual"/>
+                                subTitle="BOOK APPOINTMENT"
+                                title="Care at R K Dental Care is pleasure"
+                                description="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual"
+                            />
 
                             <ContactForm />
                         </div>
--- src/sections/Team/Team.js
+++ src/sections/Team/Team.js
@@ -28,40 +28,40 @@ const Team = () => {
     ]
 
 
-    return (
-        <section className='team-section pt-100' data-aos="fade-up" data-aos-duration="2000">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-7">
-                        <SectionTitle 
-                            subTitle="Meet our Team"
-                            title="Get to know the dentalist
-                            dental Team"
-                        />
-                    </div>
+    // return (
+        // <section className='team-section pt-100' data-aos="fade-up" data-aos-duration="2000">
+        //     <div className="container">
+        //         <div className="row">
+        //             <div className="col-lg-7">
+        //                 <SectionTitle 
+        //                     subTitle="Meet our Team"
+        //                     title="Get to know the dentalist
+        //                     dental Team"
+        //                 />
+        //             </div>
 
-                    <div className="col-lg-5">
-                        <p className='pt-5'>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-                    </div>
-                </div>
+        //             <div className="col-lg-5">
+        //                 <p className='pt-5'>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+        //             </div>
+        //         </div>
 
-                <div className="row">
-                    {
-                        teams.map (team => 
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="team-card">
-                                    <div className="team-img">
-                                        <img src={team.img} alt="" />
-                                    </div>
-                                    <h3>{team.name}</h3>
-                                </div>
-                            </div>
-                        )
-                    }
-                </div>
-            </div>
-        </section>
-    );
+        //         <div className="row">
+        //             {
+        //                 teams.map (team => 
+        //                     <div className="col-lg-3 col-sm-6">
+        //                         <div className="team-card">
+        //                             <div className="team-img">
+        //                                 <img src={team.img} alt="" />
+        //                             </div>
+        //                             <h3>{team.name}</h3>
+        //                         </div>
+        //                     </div>
+        //                 )
+        //             }
+        //         </div>
+        //     </div>
+        // </section>
+    // );
 };
 
 export default Team;
